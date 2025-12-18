d6413bb5dbc8558e68101ceafed9c194a9612006
Sushabhan <sushabhanmajumder878@gmail.com>
2025-12-18 22:22:58 +0530

    Every thing is alright now

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,9 @@ import React from 'react'
 import LandingPage from '@/components/LandingPage';
 const page = () => {
   return (
-<<<<<<< HEAD
     <main>
       <LandingPage/>
     </main>
-=======
-    <div>
-     COMPOSIT 2026 website under construction!
-     Stay tuned for updates.
-    </div>
->>>>>>> 8ab941a0f84a3e161aca919c62dd67f789c80ec3
   )
 }
 
