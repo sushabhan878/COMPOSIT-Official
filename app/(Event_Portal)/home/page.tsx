7c9ca4480717c2013455dbd5b6509183d0bcc016
Sushabhan <sushabhanmajumder878@gmail.com>
2025-12-22 17:05:03 +0530

    page layout check

--- app/(Event_Portal)/home/page.tsx
+++ app/(Event_Portal)/home/page.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <main>
+    <main className='flex items-center justify-center min-h-screen'>
+      <div className='text-center'>COMPOSIT 2026</div>
     </main>
   )
 }
